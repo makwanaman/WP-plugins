8dc5c1e8b1cb5824cf4293b1f417b0210880d501
Aman <makwanaman2@gmail.com>
2023-06-09 01:12:21 +0530

    nothing

--- view/WPfavs-lists/index.js
+++ view/WPfavs-lists/index.js
@@ -8,7 +8,7 @@ const index = () => {
   return (
     <>
       <div className="page-Header">
-        <Header />
+         <Header /> 
         <EntryHeader />
       </div>
       <FavlistTemplate />
